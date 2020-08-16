22302e97cdd5dae2dee0f444417b39c5a85ce793
Soybean Deaf <soybeandeaf@gmail.com>
2020-08-16 09:07:26 +0100

    Test

---
+++ src/plant_water/model/plant.py
@@ -0,0 +1,26 @@
+import logging
+
+from typing import Optional
+from datetime import datetime, timedelta
+
+class Plant:
+    def __init__(self,
+                 name: str,
+                 common_name: str,
+                 species_name: str,
+                 planted_on: datetime,
+                 last_watered: Optional[datetime] = None,
+                 moisture_level: Optional[int] = None):
+        self.logger = logging.getLogger(__name__)
+        self.name = name
+        self.common_name = common_name
+        self.species_name = species_name
+        self.planted_on = planted_on
+        self.last_watered = last_watered
+        self.moisture_level = moisture_level
+
+    @property
+    def time_since_watered(self) -> timedelta:
+        if self.last_watered:
+            return datetime.now() - self.last_watered
+        return self.planted_on
---
+++ src/plant_water/services/sensor.py
@@ -0,0 +1,20 @@
+from paho.mqtt.client import Client, MQTTMessage # type: ignore
+from typing import Any, List
+
+from plant_water.model.sensor import Sensor
+from .mqtt import MQTTMessageService
+
+class SensorService:
+    def __init__(self, mqtt_message_service: MQTTMessageService):
+        self.sensors: List[Sensor] = []
+        self.mqtt_message_service = mqtt_message_service
+        self.mqtt_message_service.subscribe("sensors/moisture/#", self._handle_moisture_reading)
+
+    def _handle_moisture_reading(self, client: Client, userdata: Any, message: MQTTMessage):
+        sensor = self.sensors[message.topic]
+        print(sensor)
+
+    def _get_sensor_id_from_topic(self, topic: str) -> int:
+        pass
+
+        
